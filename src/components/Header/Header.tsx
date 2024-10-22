20148ffe066e05e09f355f14ce1d8c373f362634
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2024-10-22 16:32:18 +0200

    Better Project Structure
    Project has a more better structure, better scalability and it is more readable

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,25 +5,30 @@ const Header: React.FC = () => {
     <header className="text-center">
       <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top p-3 flex items-center justify-between">
         <div className="center-slogan">
-          <a href="#" className="navbar-brand logo">
-            <img
-              src="https://see.fontimg.com/api/rf5/G3lEZ/NTEyNGQ1ODUyMjQzNDQ3MGI4M2Q3NzM4YTkwYmZkYzgub3Rm/Y2hpY2sgY29tcGFyZQ/st-noodles-grotesque.png?r=fs&h=65&w=1000&fg=000000&bg=FFFFFF&tb=1&s=65"
-              alt="Advertising fonts"
-            />
-          </a>
-          <p>Peck Out the Best Deals</p>
+            <div className="logo">
+                <a href="#" className="navbar-brand">
+                    <img
+                    src="https://see.fontimg.com/api/rf5/G3lEZ/NTEyNGQ1ODUyMjQzNDQ3MGI4M2Q3NzM4YTkwYmZkYzgub3Rm/Y2hpY2sgY29tcGFyZQ/st-noodles-grotesque.png?r=fs&h=65&w=1000&fg=000000&bg=FFFFFF&tb=1&s=65"
+                    alt="Advertising fonts"
+                    />
+                </a>
+                <p>Peck Out the Best Deals</p>
+            </div>
+            <div className="nav-btn-toggle">
+                <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+       
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
